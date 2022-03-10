fe15a921447b354df4e755292f4193e1fd35b9eb
HumSos <humberto@DESKTOP-EN3DQET.localdomain>
2022-03-09 18:56:26 -0600

    solution update

--- src/pick_place/src/solution_template.py
+++ src/pick_place/src/solution_template.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from pickle import TRUE
 import rospy
 import sys
 import tf_conversions
@@ -15,10 +16,57 @@ class Planner():
 
   def __init__(self):
     #TODO: Initialise move it interface
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('TaskSolution', anonymous=True)
+    robot = moveit_commander.RobotCommander()
+    scene = moveit_commander.PlanningSceneInterface()
+    group_name = "xarm6" 
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=20)
+    print("============ Printing robot state")
+    print(robot.get_current_state())
+    print("")
+
+    planning_frame = move_group.get_planning_frame()
+    eef_link = move_group.get_end_effector_link()
+    group_names = robot.get_group_names()
+
+    # Misc variables 
+    self.box_name = ""
+    self.robot = robot
+    self.scene = scene
+    self.move_group = move_group
+    self.display_trajectory_publisher = display_trajectory_publisher
+    self.planning_frame = planning_frame
+    self.eef_link = eef_link
+    self.group_names = group_names
+
 
   def wait_for_state_update(self,box_name, box_is_known=False, box_is_attached=False, timeout=0.5):
 
     #TODO: Whenever we change something in moveit we need to make sure that the interface has been updated properly
+    start = rospy.get_time()
+    seconds = rospy.get_time()
+    while (seconds - start < timeout) and not rospy.is_shutdown():
+      # Test if the box is in attached objects
+      attached_objects = self.scene.get_attached_objects([box_name])
+      is_attached = len(attached_objects.keys()) > 0
+
+      # Test if the box is in the scene.
+      # Note that attaching the box will remove it from known_objects
+      is_known = self.box_name in self.scene.get_known_object_names()
+
+      # Test if we are in the expected state
+      if (box_is_attached == is_attached) and (box_is_known == is_known):
+        return True
+
+      # Sleep so that we give other threads time on the processor
+      rospy.sleep(0.1)
+      seconds = rospy.get_time()
+
+      # If we exited the while loop without returning then we timed out
+      return False
+      
 
   def addObstacles(self):
 
@@ -37,6 +85,18 @@ class Planner():
   def goToPose(self,pose_goal):
 
     #TODO: Code used to move to a given position using move it
+    pose_goal = geometry_msgs.msg.Pose()
+    pose_goal.orientation.w = 1.0
+    pose_goal.position.x = 0.4
+    pose_goal.position.y = 0.1
+    pose_goal.position.z = 0.4
+
+    self.move_group.set_pose_targets(pose_goal)
+    self.move_group.go(wait= TRUE)
+    #move_group.execute(plan, wait=True)
+    self.move_group.stop()
+    self.move_group.clear_pose_targets()
+    
 
 
   def detachBox(self,box_name):
